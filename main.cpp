d65da7991317836b1425aa95f07cf4877457659a
nguyenquocdat13062007-netizen <nguyenquocdat13062007@gmail.com>
2026-07-08 13:46:46 +0700

    Refactor main function for better string processing
    Updated main function to improve user interaction and string handling.

--- main.cpp
+++ main.cpp
@@ -1,47 +1,52 @@
 #include "MyString.h"
 #include <iostream>
-
 using namespace std;
 
-void main() {
-	MyString str1, str2, str3;
-	cout << "Nhap chuoi 1" << endl;
-	cin >> str1;
+int main() {
+    MyString str1, str2, str3;
+    cout << "---------------------------------PLEASE ENTER 2 STRINGS---------------------------------" << "\n\n";
+    cout << "Enter string 1: ";
+    cin >> str1;
+    cout << "Enter string 2: ";
+    cin >> str2;
+    cout << "\n\n";
+    cout << "---------------------------------ASSIGN STR1'S VALUE TO STR3---------------------------------" << "\n\n";
+    str3 = str1;
+    cout << "String 3 after assignment: " << str3 << "\n\n";
+    cout << "---------------------------------COMPARE STR1 AND STR2---------------------------------" << "\n\n";
+    if (str1 > str2)  cout << "String 1 is greater than string 2" << "\n";
+    else if (str1 < str2) cout << "String 1 is less than string 2" << "\n";
+    else cout << "String 1 is equal to string 2" << "\n";
 
-	cout << "nhap chuoi 2" << endl;
-	cin >> str2;
+    if (str1 == str2) cout << "String 1 is equal to string 2" << "\n";
+    else cout << "String 1 is different from string 2" << "\n";
 
-	cout << "Nhap chuoi 3" << endl;
-	cin >> str3;
+    if (str1 != str2) cout << "String 1 is different from string 2" << "\n";
+    else cout << "String 1 is equal to string 2" << "\n";
 
-	cout << "Chuoi 1: " << str1 << endl;
-	cout << "chuoi 2: " << str2 << endl;
-	cout << "Chuoi 3: " << str3 << endl;
+    if (str1 >= str2) cout << "String 1 is greater than or equal to string 2" << "\n";
+    else cout << "String 1 is less than string 2" << "\n";
 
-	cout << "Chuoi 3 = chuoi 1" << endl;
-	str3 = str1;
+    if (str1 <= str2) cout << "String 1 is less than or equal to string 2" << "\n\n";
+    else cout << "String 1 is greater than string 2" << "\n\n";
 
-	cout << "So sanh: chuoi 3 = chuoi 2: " << (str3 == str2 ? "Dung" : "Sai") << endl;
-	cout << "So sanh: chuoi 3 != chuoi 2: " << (str3 != str2 ? "Dung" : "Sai") << endl;
-	cout << "So sanh: chuoi 3 > chuoi 2: " << (str3 > str2 ? "Dung" : "Sai") << endl;
-	cout << "So sanh: chuoi 3 <= chuoi 2: " << (str3 <= str2 ? "Dung" : "Sai") << endl;
-	cout << "So sanh: chuoi 3 < chuoi 2: " << (str3 < str2 ? "Dung" : "Sai") << endl;
-	cout << "So sanh: chuoi 3 >= chuoi 2: " << (str3 >= str2 ? "Dung" : "Sai") << endl;
+    cout << "---------------------------------PLEASE ENTER THE DESIRED STRING SIZE---------------------------------" << "\n\n";
+    cout << "Enter desired size: ";
+    int n;
+    cin >> n;
+    str3.set(n);
+    cout << "String 3 after resizing: " << str3 << "\n\n";
 
+    cout << "---------------------------------PLEASE ENTER THE DESIRED STRING---------------------------------" << "\n\n";
+    MyString str4;
+    cout << "Enter desired string: ";
+    cin >> str4;
+    str3.set(str4);
+    cout << "String 3 after setting string: " << str3 << "\n\n";
+    cout << "---------------------------------RESULT AFTER CONCATENATION---------------------------------" << "\n\n";
+    MyString kq;
+    kq = "T" + str1 + str2 + " " + str3;
+    cout << "T" << " + str1 + str2 + 'space' + str3: " << kq;
 
-	cout << "Nhap so - set" << endl;
-	int n; cin >> n;
-	str3.set(n);
-	cout << "Chuoi 3 - set so: " << str3 << endl;
-
-	cin.ignore();
-	cout << "Nhap chuoi - set" << endl;
-	char temp[100];
-	cin.getline(temp, 100);
-	str3.set((const char*)temp);
-	cout << "Chuoi 3 - set chuoi: " << str3 << endl;
-
-	MyString kq = "T" + str1 + str2 + " " + str3;
-	cout << "kq: " << kq << endl;
-	return;
+    return 0;
 }
