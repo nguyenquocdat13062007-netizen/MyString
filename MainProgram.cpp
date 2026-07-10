e2c050bfc1e1cb37167c2686c66f88bc6c1900bd
nguyenquocdat13062007-netizen <nguyenquocdat13062007@gmail.com>
2026-07-10 10:12:19 +0700

    Implement Insertion Sort in SortAlg class

---
+++ MainProgram.cpp
@@ -0,0 +1,42 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class SortAlg
+{
+private:
+	SortAlg() {};
+	static void (*currentAlg)(float[], int);
+public:
+	static void SetAlg(void (*alg)(float[], int))
+	{
+		if (alg != NULL)
+		{
+			currentAlg = alg;
+		}
+	};
+	static void BubbleSort(float[], int);
+	static void SelectionSort(float[], int);
+	static void InsertionSort(float[], int);
+	static void InterchangeSort(float[], int);
+	static void Sort(float[], int);
+	void (*menuAlg[])(float[], int);
+};
+void SortAlg::InsertionSort(float arr[], int n) {
+	int j;
+	float pos;
+	for (int i = 1; i < n; i++) {
+		pos = arr[i];
+		j = i - 1;
+
+		while (j >= 0) {
+			if (arr[j] > pos) {
+				arr[j + 1] = arr[j];
+				j = j - 1;
+			}
+			else {
+				break;
+			}
+		}
+		arr[j + 1] = pos;
+	}
+}
