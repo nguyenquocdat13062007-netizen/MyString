9b6510ad318b4eb12415e7e97c0daf6eadf4c77d
nguyenquocdat13062007-netizen <nguyenquocdat13062007@gmail.com>
2026-07-10 10:13:06 +0700

    Refactor SortAlg class and add sorting methods
    Refactored SortAlg class to include SetAlg method and added InterchangeSort method.

--- SortAgl.h
+++ SortAgl.h
@@ -1,15 +1,45 @@
 #pragma once
+#include <iostream>
+using namespace std;
 class SortAlg
 {
 private:
 	SortAlg() {};
-	void (*currentAlg)(float[], int);
+	static void (*currentAlg)(float[], int);
 public:
-	static void Menu(void (*alg)(float[], int));
-	static void BubbleSort(float[], int n);
-	static void SelectionSort(float[], int n);
-	static void InsertionSort(float[], int n);
+	static void SetAlg(void (*alg)(float[], int))
+	{
+		if (alg != NULL)
+		{
+			currentAlg = alg;
+		}
+	};
+	static void BubbleSort(float[], int);
+	static void SelectionSort(float[], int);
+	static void InsertionSort(float[], int);
+	static void InterchangeSort(float[], int);
+	static void Sort(float[], int);
+	void (*menuAlg[])(float[], int);
 };
+void SortAlg::InsertionSort(float arr[], int n) {
+	int j;
+	float pos;
+	for (int i = 1; i < n; i++) {
+		pos = arr[i];
+		j = i - 1;
+
+		while (j >= 0) {
+			if (arr[j] > pos) {
+				arr[j + 1] = arr[j];
+				j = j - 1;
+			}
+			else {
+				break;
+			}
+		}
+		arr[j + 1] = pos;
+	}
+}
 
 void SortAlg::BubbleSort(float a[], int n) {
         for (int i = 0; i < n - 1; i++) {
