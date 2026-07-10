3b79d5b7a4c22b846f89e140bc62270f0b4ad7a0
giabao1001 <146498515+giabao1001@users.noreply.github.com>
2026-07-10 09:41:37 +0700

    bubbleSort
    Implemented the BubbleSort function in SortAlg class.

--- SortAgl.h
+++ SortAgl.h
@@ -10,3 +10,16 @@ public:
 	static void SelectionSort(float[], int n);
 	static void InsertionSort(float[], int n);
 };
+
+void SortAlg::BubbleSort(float a[], int n) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    // Đổi chỗ
+                    float temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+    }
